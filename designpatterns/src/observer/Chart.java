86e1678310b2e274b16156fc0f58e7acc231aff5
seanarch <zhuqingyun@outlook.com>
2023-11-06 15:02:24 -0700

    Obs pattern: push style communication

--- designpatterns/src/observer/Chart.java
+++ designpatterns/src/observer/Chart.java
@@ -2,7 +2,7 @@ package observer;
 
 public class Chart implements Observer{
     @Override
-    public void update() {
-        System.out.println("Chart got updated");
+    public void update(int value) {
+        System.out.println("Chart got updated: " + value);
     }
 }
