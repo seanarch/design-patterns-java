5939b97150a0a6386abb9b68b52c0ea83ca1965f
seanarch <zhuqingyun@outlook.com>
2023-11-06 15:15:48 -0700

    pull style communication

--- designpatterns/src/App.java
+++ designpatterns/src/App.java
@@ -77,9 +77,9 @@ public class App {
         System.out.println(document.getContent());
 
         var dataSource = new DataSource();
-        var sheet1 = new SpreadSheet();
-        var sheet2 = new SpreadSheet();
-        var chart = new Chart();
+        var sheet1 = new SpreadSheet(dataSource);
+        var sheet2 = new SpreadSheet(dataSource);
+        var chart = new Chart(dataSource);
 
         dataSource.addObserver(sheet1);
         dataSource.addObserver(sheet2);
--- designpatterns/src/observer/Observer.java
+++ designpatterns/src/observer/Observer.java
@@ -1,5 +1,5 @@
 package observer;
 
 public interface Observer {
-    void update(int value);
+    void update();
 }
